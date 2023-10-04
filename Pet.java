2628ed9867db74da11f5a1b34e37ededd206090b
QiuRongChen2023 <121112820+QiuRongChen2023@users.noreply.github.com>
2023-10-04 13:46:28 -0400

    Add files via upload

---
+++ Pet.java
@@ -0,0 +1,80 @@
+package abstract_classes_and_interfaces;
+
+public abstract class Pet implements FoodEater,Communicator{
+	
+	private String favoriteFood;
+	private String favoriteToy;
+	private int maintenanceLevel;
+	private boolean goesOutside;
+	
+	public Pet() {
+		favoriteFood = "unknow food";
+		favoriteToy = "unknow toy";
+		maintenanceLevel = 0;
+		goesOutside = false;
+	}
+	
+	public Pet(String faveFood, String faveToy, int maintLev, boolean goesOut) {
+		this();
+		favoriteFood = faveFood;
+		favoriteToy = faveToy;
+		maintenanceLevel = maintLev;
+		goesOutside = goesOut;
+	}
+	
+	public abstract void play(); 
+	
+	public String getFavoriteFood() {
+		return favoriteFood;
+	}
+	
+	public String getFavoriteToy() {
+		return favoriteToy;
+	}
+	
+	public int getMaintenanceLevel() {
+		return maintenanceLevel;
+	}
+	
+	public boolean isGoesOutside() {
+		return goesOutside;
+	}
+	
+	public void setFavoriteFood(String favoriteFood) {
+		this.favoriteFood = favoriteFood;
+	}
+	
+	public void setFavoriteToy(String favoriteToy) {
+		this.favoriteToy = favoriteToy;
+	}
+	
+	public void setMaintenanceLevel(int maintenanceLevel) {
+		this.maintenanceLevel = maintenanceLevel;
+	}
+	
+	public void setGoesOutside(boolean goesOutside) {
+		this.goesOutside = goesOutside;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
