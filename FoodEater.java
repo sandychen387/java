61b7dda605fea9d29cc3199b75c1809bded12fbd
QiuRongChen2023 <121112820+QiuRongChen2023@users.noreply.github.com>
2023-10-04 13:47:17 -0400

    Add files via upload

---
+++ FoodEater.java
@@ -0,0 +1,12 @@
+package abstract_classes_and_interfaces;
+
+public interface FoodEater {
+	
+	static final double METABOLISM_RATING_FAST =.75;
+	static final double METABOLISM_RATING_MEDIUM = .50;
+	static final double METABOLISM_RATING_SLOW =.25;
+	
+	void eat();
+	void eat(Food f);
+	double metabolizeFood(Food f);
+}
