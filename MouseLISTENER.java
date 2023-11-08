0abf87e54dd05471f284e8e8abb4333e511acef9
QiuRongChen2023 <121112820+QiuRongChen2023@users.noreply.github.com>
2023-11-07 19:37:16 -0500

    Add files via upload

---
+++ MouseLISTENER.java
@@ -0,0 +1,15 @@
+import java.awt.event.MouseEvent;
+
+public interface MouseLISTENER {
+
+	void mouseClicked(MouseEvent e);
+
+	void mousePressed(MouseEvent e);
+
+	void mouseReleased(MouseEvent e);
+
+	void mouseEntered(MouseEvent e);
+
+	void mouseExited(MouseEvent e);
+
+}
